55736c03f66de11017a532ee58c85b0efd92d78e
QuocAnU <an.lequoc2103@hcmut.edu.vn>
2024-02-28 13:57:41 +0700

    Add

---
+++ BackEnd/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { json } from "express";
+import { PORT } from "./env.js";
+import cors from "cors";
+
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("Hello")
+})
+
+app.listen(PORT, () => {
+    console.log(`App is running in PORT ${PORT}`);
+});
+
