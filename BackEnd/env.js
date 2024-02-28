550741987f4c6edc673f34c42d3792103ddad75c
Thái Bảo Long <subin27102003@gmail.com>
2024-02-28 15:18:08 +0700

    Connect to db

--- BackEnd/env.js
+++ BackEnd/env.js
@@ -1,1 +1,3 @@
 export const PORT = 3001;
+export const db =
+  "mongodb+srv://longthai2710:4inE4nkD1x2Fu7nG@cluster0.z0u8mza.mongodb.net/";
--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,17 +2,18 @@ import express from "express";
 import { json } from "express";
 import { PORT } from "./env.js";
 import cors from "cors";
+import Database from "./configs/database.js";
 
-
+const db = new Database();
+db.connect();
 const app = express();
 app.use(json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("Hello")
-})
-
-app.listen(PORT, () => {
-    console.log(`App is running in PORT ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Hello22344");
 });
 
+app.listen(PORT, () => {
+  console.log(`App is running in PORT ${PORT}`);
+});
