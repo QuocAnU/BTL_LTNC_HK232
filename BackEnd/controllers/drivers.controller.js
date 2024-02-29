69352d6952c41a47991875bf0ada12fc9cbc6f54
QuocAnU <an.lequoc2103@hcmut.edu.vn>
2024-03-01 01:15:52 +0700

    jwt authentication login history ...

--- BackEnd/controllers/drivers.controller.js
+++ BackEnd/controllers/drivers.controller.js
@@ -1,11 +1,14 @@
+import { verifyToken } from "../middleware/jwtAuthentication.js";
+import { Trip } from "../models/Trip.js";
 import { Vehicle } from "../models/Vehicle.js";
 import { Drivers, DriversManager } from "../models/driver.model.js";
 
 export const getAllDrivers = async (req, res) => {
     try {
-        const allDrivers = await Drivers.find();
-        console.log(allDrivers)
-        return res.status(200).send(allDrivers);
+        verifyToken(req, res, async () => {
+            const allDrivers = await Drivers.find();
+            return res.status(200).send(allDrivers);
+        })
     } catch (error) {
         console.error(error.message);
         res.status(500).send({ message: "Internal Server Error" });
@@ -15,60 +18,102 @@ export const getAllDrivers = async (req, res) => {
 export const addDrivers = async (req, res) => {
 
     try {
-        const driverData = req.body
-        const driver = await DriversManager.addDriver(driverData);
-        // if (req.body.ids_car) {
-        //     await Vehicle.updateMany({ _id: { $in: req.body.ids_car } }, { $set: { status: "on" } });
-        // }
-        // console.log("Updated status vihicle successfully");
-        return res.status(201).send({
-            driver: driver,
-            message: "Driver deleted successfully"
-        });
+        verifyToken(req, res, async () => {
+            let driverData = req.body;
+            const requiredFields = ['names', 'phone', 'address', 'license', 'totaldistance', 'statusD', 'ids_car', 'deleted'];
+            const missingFields = requiredFields.filter(field => !(field in driverData));
+            if (missingFields.length > 0) {
+                return res.status(400).send({ message: `Missing required fields: ${missingFields.join(', ')}` });
+            }
+            let lastDriver = await Drivers.findOne({}, {}, { sort: { 'STT': -1 } });
+            let STT = (lastDriver.STT) + 1;
+            driverData = { ...driverData, STT: STT };
+            const driver = await DriversManager.addDriver(driverData);
+            if (req.body.ids_car) {
+                await Vehicle.updateMany({ ids: { $in: req.body.ids_car } }, { $set: { status: "on" } });
+            }
+            console.log("Updated status vihicle successfully");
+            return res.status(201).send({
+                driver: driver,
+                message: "Driver deleted successfully"
+            });
+        })
+
     } catch (error) {
         console.error(error.message);
         res.status(500).send({ message: "Internal Server Error" });
     }
 }
 
-export const updateDrivers = async (req, res) => {
+export const updatedDrivers = async (req, res) => {
     try {
-        const { id } = req.params;
-        const { status, ...updatedData } = req.body;
-        const updatedDriver = await DriversManager.updatedDriver(id, updatedData);
+        verifyToken(req, res, async () => {
+            const { id } = req.params;
+            const { status, ...updatedData } = req.body;
+            const requiredFields = ['STT', 'names', 'phone', 'address', 'license', 'totaldistance', 'statusD', 'ids_car', 'deleted'];
+            const missingFields = requiredFields.filter(field => !(field in updatedData));
+            if (missingFields.length > 0) {
+                return res.status(400).send({ message: `Missing required fields: ${missingFields.join(', ')}` });
+            }
+            const oldDriver = await Drivers.findById(id);
+            const updatedDriver = await DriversManager.updatedDriver(id, updatedData);
+            console.log("O", oldDriver.ids_car);
+            console.log("N", updatedDriver.ids_car);
+            console.log("ST", status);
 
-        // if (req.body.ids_car && req.body.ids_car !== updatedDriver.ids_car) {
-        //     // Cập nhật trạng thái của các xe trong ids_car cũ thành "maintain" hoặc "off"
-        //     await Vehicle.updateMany({ _id: { $in: updatedDriver.ids_car } }, { $set: { status: status } });
+            if (oldDriver.ids_car !== updatedDriver.ids_car) {
+                // Cập nhật trạng thái của các xe trong ids_car cũ thành "maintain" hoặc "off"
+                await Vehicle.updateMany({ ids: { $in: oldDriver.ids_car } }, { $set: { status: status } });
+                console.log("Updated status old vihicle successfully");
+                // Cập nhật trạng thái của các xe mới trong ids_car thành "on"
+                await Vehicle.updateMany({ ids: { $in: updatedDriver.ids_car } }, { $set: { status: "on" } });
+                console.log("Updated status new vihicle successfully");
+            }
 
-        //     // Cập nhật trạng thái của các xe mới trong ids_car thành "on"
-        //     await Vehicle.updateMany({ _id: { $in: req.body.ids_car } }, { $set: { status: "on" } });
-        // }
+            return res.status(201).send({
+                updatedDriver: updatedDriver,
+                message: "Driver updated successfully"
+            }
+            );
+        })
 
-        return res.status(201).send({
-            updatedDriver: updatedDriver,
-            message: "Driver updated successfully"
-        }
-        );
     } catch (error) {
         console.error(error.message);
         res.status(500).send({ message: "Internal Server Error" });
     }
 }
 
-export const deleteDrivers = async (req, res) => {
+export const deletedDrivers = async (req, res) => {
     try {
-        const { id } = req.params;
-        const deleted = await DriversManager.deletedDriver(id);
+        verifyToken(req, res, async () => {
+            const { id } = req.params;
+            const deleted = await DriversManager.deletedDriver(id);
 
-        console.log("Driver deleted:");
-        // await Vehicle.updateMany({ _id: { $in: driver.ids_car } }, { $set: { status: "off" } });
-        // console.log("Updated status vihicle successfully");
+            console.log("Driver deleted:");
+            await Vehicle.updateMany({ ids: { $in: deleted.ids_car } }, { $set: { status: "off" } });
+            console.log("Updated status vihicle successfully");
 
-        return res.status(200).send({
-            deleted: deleted,
-            message: "Driver deleted successfully"
-        });
+            return res.status(200).send({
+                deleted: deleted,
+                message: "Driver deleted successfully"
+            });
+        })
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send({ message: "Internal Server Error" });
+    }
+};
+
+export const getHistoryDrivers = async (req, res) => {
+    try {
+        verifyToken(req, res, async () => {
+            const { id } = req.params;
+            const driversById = await Drivers.findById(id);
+            const historyDrivers = await Trip.find({ ids_driver: driversById.STT })
+            return res.status(200).send(historyDrivers);
+        })
+
     } catch (error) {
         console.error(error.message);
         res.status(500).send({ message: "Internal Server Error" });
