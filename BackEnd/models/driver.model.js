2410eea7c618ed891ddcac1105d5456d976e0f08
QuocAnU <an.lequoc2103@hcmut.edu.vn>
2024-02-29 16:39:03 +0700

    CRUD driver

--- BackEnd/models/driver.model.js
+++ BackEnd/models/driver.model.js
@@ -5,13 +5,14 @@ const driverSchema = new mongoose.Schema({
     STT: {
         type: Number,
         required: true,
+        unique: true
     },
-    name: {
+    names: {
         type: String,
         required: true,
     },
     phone: {
-        type: Number,
+        type: String,
         required: true,
     },
     address: {
@@ -21,11 +22,62 @@ const driverSchema = new mongoose.Schema({
     license: {
         type: String,
         required: true,
+    },
+    totaldistance: {
+        type: Number,
+        required: true,
+    },
+    statusD: {
+        type: String,
+        required: true,
+    },
+    ids_car: {
+        type: String,
+        required: true,
+    },
+    deletes: {
+        type: Boolean,
+        required: true,
     }
 });
 
-export const drivers = mongoose.model("Drivers", driverSchema);
+const Drivers = mongoose.model("Drivers", driverSchema);
 
-
-
+class DriversManager {
+    static async addDriver(driverdata) {
+        try {
+            const driver = new Drivers(driverdata);
+            await driver.save();
+            console.log("New driver saved:");
+            return driver;
+        } catch (error) {
+            console.error(error.message);
+            throw new Error("Failed to add driver");
+        }
+    }
+    static async updatedDriver(id, driverdata) {
+        try {
+            const updated = await Drivers.findByIdAndUpdate(id, driverdata, { new: true });
+            if (!updated) {
+                return res.status(404).send({ message: "Driver not found" });
+            }
+            console.log("Driver updated:");
+            return updated;
+        }
+        catch (error) {
+            console.error(error.message);
+            throw new Error("Failed to update driver");
+        }
+    }
+    static async deletedDriver(id) {
+        const driver = await Drivers.findById(id);
+        if (!driver) {
+            return res.status(404).send({ message: "Driver not found" });
+        }
+        driver.deletes = true;
+        await driver.save();
+        return driver;
+    }
+}
+export { Drivers, DriversManager };
 
