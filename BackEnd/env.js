fe2c122ca1020078fb97dd6e26320f59151e0ac4
khoa <khicondangyeu490@gmail.com>
2024-04-15 21:56:13 +0700

    ffk

--- BackEnd/env.js
+++ BackEnd/env.js
@@ -1,4 +1,4 @@
 export const PORT = 3001;
 export const db =
-  "mongodb+srv://longthai2710:4inE4nkD1x2Fu7nG@cluster0.z0u8mza.mongodb.net/mykar";
+    "mongodb+srv://longthai2710:4inE4nkD1x2Fu7nG@cluster0.z0u8mza.mongodb.net/mykar?retryWrites=true&w=majority";
 
