ce2ca192cb33d13627f0fcf3b3c16f0919f23d83
cosimaa <vandoto123@gmail.com>
2024-05-05 22:07:19 +0700

    Fix Header

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -9,7 +9,6 @@ import LocalShippingSharpIcon from "@mui/icons-material/LocalShippingSharp";
 import AirlineSeatReclineExtraSharpIcon from "@mui/icons-material/AirlineSeatReclineExtraSharp";
 import SupportAgentIcon from "@mui/icons-material/Search";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import BasicMenu from "./Menuprofile/Menuprofile";
 const cx = classNames.bind(styles);
 function Header() {
   const handleBackHome = () => {
@@ -18,45 +17,10 @@ function Header() {
   return (
     <div className={cx("wrapper")}>
       <div className={cx("header1")} onClick={handleBackHome}>
-        {/* <img src={logo} alt="logo" className={cx("logo")}></img> */}
+        {}
         <img src={title} alt="title" className={cx("title")}></img>
       </div>
-      {/* <div className={cx("action")}>
-        <div>
-          <HomeIcon className={cx("action-icon")} sx={{ fontSize: 40 }} />
-          <div className={cx("active")}></div>
-        </div>
-        <div>
-          <LocalShippingSharpIcon
-            className={cx("action-icon")}
-            sx={{ fontSize: 40 }}
-          />
-          <div className={cx("active")}></div>
-        </div>
-        <div>
-          <AirlineSeatReclineExtraSharpIcon
-            className={cx("action-icon")}
-            sx={{ fontSize: 40 }}
-          />
-          <div className={cx("active")}></div>
-        </div>
-        <div>
-          <FormatListBulletedIcon className={cx("action-icon")} sx={{ fontSize: 40 }} />
-          <div className={cx("active")}></div>
-        </div>
-      </div>
-      <div className={cx("search")}>
-        <h3 className={cx("search-icon1")}>Search by ID or name</h3>
-        <SupportAgentIcon
-          className={cx("search-icon1")}
-          sx={{ fontSize: 30}}
-        />
-      </div>
-      <BasicMenu>
-        <AccountCircleIcon className={cx("search-icon2")} sx={{ fontSize: 40 }}>
-          <AccountCircleIcon />
-        </AccountCircleIcon>
-      </BasicMenu> */}
+      {}
     </div>
   );
 }
